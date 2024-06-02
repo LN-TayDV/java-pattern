a41f6eecd7ae8bae275a5ec35df5b052e3d82f97
TayDang <dangvantay9686@gmail.com>
2024-06-02 16:35:34 +0700

    cmm

--- 01_creational/monostate/src/main/java/com/iluwatar/monostate/LoadBalancer.java
+++ 01_creational/monostate/src/main/java/com/iluwatar/monostate/LoadBalancer.java
@@ -46,7 +46,7 @@ public class LoadBalancer {
         }
     }
 
-    /*
+    /**
      * Tại sao sử dụng synchronized?
      * Tránh điều kiện race (race condition):
      * Điều kiện race xảy ra khi nhiều luồng truy cập và thay đổi cùng một tài nguyên chia sẻ
@@ -87,7 +87,6 @@ public class LoadBalancer {
         synchronized (SERVERS) {
             SERVERS.add(server);
         }
-
     }
 
     public final int getNoOfServers() {
@@ -99,7 +98,37 @@ public class LoadBalancer {
     }
 
     /**
-     * Handle request.
+     * 1. Synchronized Method:
+     *     - Phương thức `serverRequest` trong lớp `LoadBalancer` được đánh dấu là `synchronized`,
+     *     nghĩa là chỉ một luồng có thể thực hiện nó tại một thời điểm.
+     *     Khi một luồng đang thực hiện phương thức này, các luồng khác cần phải chờ đợi
+     *     cho đến khi khóa `synchronized` được giải phóng trước khi chúng có thể truy cập vào phương thức.
+     *
+     * 2. Chia Sẻ Biến `lastServedId`:
+     *     - Biến `lastServedId` được khai báo là biến static,
+     *     nghĩa là nó được chia sẻ giữa tất cả các thể hiện của lớp `LoadBalancer`.
+     *     Khi một thể hiện thay đổi giá trị của `lastServedId`,
+     *     thay đổi này sẽ ảnh hưởng đến tất cả các thể hiện khác.
+     *
+     * 3. Vấn Đề Thực Hiện Đồng Thời:
+     *     - Trong phương thức `main`, bạn đã tạo hai thể hiện của `LoadBalancer`
+     *     và gọi phương thức `serverRequest` từ mỗi thể hiện.
+     *     Dù là hai thể hiện, nhưng do phương thức `serverRequest` là `synchronized`,
+     *     nên chỉ một trong số chúng được phép thực hiện phương thức này tại một thời điểm.
+     *
+     *     - Khi một thể hiện của `LoadBalancer` thực hiện phương thức `serverRequest`và đang giữ khóa `synchronized`,
+     *     thì thể hiện khác sẽ phải chờ đợi cho đến khi khóa được giải phóng
+     *     trước khi nó có thể truy cập vào phương thức `serverRequest`.
+     *
+     * 4. Kết Quả:
+     *     - Do chỉ một thể hiện có thể thực hiện phương thức `serverRequest` tại một thời điểm,
+     *     nên khi một thể hiện tăng giá trị của `lastServedId`,
+     *     thì giá trị mới của `lastServedId` sẽ được sử dụng bởi thể hiện khác khi nó được kích hoạt tiếp theo.
+     *
+     *     - Điều này dẫn đến việc `lastServedId` có thể tăng tiếp theo bắt đầu từ vị trí mà thể hiện trước đó đã kết thúc.
+     *
+     *    Do đó, kết quả là các yêu cầu sẽ được phân phối lần lượt cho các máy chủ theo trình tự của `lastServedId`,
+     *    dù bạn có nhiều thể hiện của lớp `LoadBalancer`.
      */
     public synchronized void serverRequest(Request request) {
         if (lastServedId >= SERVERS.size()) {
