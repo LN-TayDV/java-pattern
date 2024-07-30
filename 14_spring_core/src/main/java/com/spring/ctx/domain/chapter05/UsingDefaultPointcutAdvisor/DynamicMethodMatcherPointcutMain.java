46bb931c059776674d7d4d658055fd5dd009b18a
TayDang <dangvantay9686@gmail.com>
2024-07-30 07:02:15 +0700

    cmm

---
+++ 14_spring_core/src/main/java/com/spring/ctx/domain/chapter05/UsingDefaultPointcutAdvisor/DynamicMethodMatcherPointcutMain.java
@@ -0,0 +1,56 @@
+package com.spring.ctx.domain.chapter05.UsingDefaultPointcutAdvisor;
+
+import com.spring.ctx.domain.chapter05.UsingDefaultPointcutAdvisor.types.GoodGuitarist;
+import com.spring.ctx.domain.chapter05.UsingDefaultPointcutAdvisor.types.Singer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.DynamicMethodMatcherPointcut;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class DynamicMethodMatcherPointcutMain {
+
+    public static void main(String... args) {
+        GoodGuitarist target = new GoodGuitarist();
+        Advisor advisor = new DefaultPointcutAdvisor(new SimpleDynamicPointcut(), new SimpleAroundAdvice());
+
+        ProxyFactory pf = new ProxyFactory();
+        pf.setTarget(target);
+        pf.addAdvisor(advisor);
+
+        Singer proxy = (Singer)pf.getProxy();
+        proxy.sing("C");
+        proxy.sing("c");
+        proxy.sing("E");
+        proxy.sing();
+    }
+
+    static class SimpleDynamicPointcut extends DynamicMethodMatcherPointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return  cls -> (cls == GoodGuitarist.class);
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            return ("sing".equals(method.getName()));
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            LOGGER.info("Dynamic check for " + method.getName());
+
+            if(args.length == 0) {
+                return false;
+            }
+
+            var key = (String) args[0];
+
+            return key.equalsIgnoreCase("C");
+        }
+    }
+}
--- 14_spring_core/src/main/java/com/spring/ctx/domain/chapter05/UsingDefaultPointcutAdvisor/types/Singer.java
+++ 14_spring_core/src/main/java/com/spring/ctx/domain/chapter05/UsingDefaultPointcutAdvisor/types/Singer.java
@@ -1,6 +1,16 @@
 package com.spring.ctx.domain.chapter05.UsingDefaultPointcutAdvisor.types;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface Singer {
 
+    Logger logger = LoggerFactory.getLogger(Singer.class);
+
     void sing();
+
+    default void sing(String key){
+        logger.info("Singing in the key of {}", key);
+    }
 }
