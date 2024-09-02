21a12f35718711150f30514562b4cb97b90e1b7d
TayDang <dangvantay9686@gmail.com>
2024-09-02 18:44:24 +0700

    cmm

--- 00_algorithm/src/main/java/com/iluwatar/algorithm/theories/on/self/learning/Graph/elements/type/with/list/GraphGdge.java
+++ 00_algorithm/src/main/java/com/iluwatar/algorithm/theories/on/self/learning/Graph/elements/type/with/list/GraphGdge.java
@@ -5,4 +5,9 @@ public  interface GraphGdge<E , W> {
     Vertex<E> u() ;
 
     Vertex<E> v() ;
+
+    // Phương thức mặc định để cung cấp chuỗi mô tả về cạnh
+    default String describe() {
+        return String.format("%s -- %s", u().getTop(), v().getTop());
+    }
 }
